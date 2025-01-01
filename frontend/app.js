6c43568686ea652fc27a4c89ac535d0bc0f2bcd7
kaustubh-htw <kaustubhhtwmail@gmail.com>
2025-01-01 20:51:03 +0100

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,9 +1,9 @@
-const clientId = '2u4vfo24apguac73rovfapmebv'; // Replace with your Cognito User Pool Client ID
+const clientId = '6dj6abdhm6500vroqo6918ide6'; // Replace with your Cognito User Pool Client ID
 const redirectUri = 'https://d1tgztvbo79v27.cloudfront.net';   // Replace with your redirect URI (S3 URL)
 
 // Cognito Hosted UI URLs for login and signup
-const loginUrl = `https://gatherhub-user-pool.auth.eu-west-1.amazoncognito.com/login?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}`;
-const signupUrl = `https://gatherhub-user-pool.auth.eu-west-1.amazoncognito.com/signup?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}`;
+const loginUrl = `https://User pool - ji7fqr.auth.eu-west-1.amazoncognito.com/login?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}`;
+const signupUrl = `https://User pool - ji7fqr.auth.eu-west-1.amazoncognito.com/signup?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}`;
 
 document.getElementById('loginButton')?.addEventListener('click', () => {
     window.location.href = loginUrl;
