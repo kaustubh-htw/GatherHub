ec9450e573ca08d45c2929b530c3b6bbe8892738
kaustubh-htw <kaustubhhtwmail@gmail.com>
2025-01-01 21:03:28 +0100

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -2,8 +2,8 @@ const clientId = '6dj6abdhm6500vroqo6918ide6'; // Replace with your Cognito User
 const redirectUri = 'https://d1tgztvbo79v27.cloudfront.net';   // Replace with your redirect URI (S3 URL)
 
 // Correct Cognito Hosted UI URL format for login and signup
-const loginUrl = `https://ji7fqr.auth.eu-west-1.amazoncognito.com/login?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}`;
-const signupUrl = `https://ji7fqr.auth.eu-west-1.amazoncognito.com/signup?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}`;
+const loginUrl = `https://eu-west-1nqzilphn1.auth.eu-west-1.amazoncognito.com/login?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}`;
+const signupUrl = `https://eu-west-1nqzilphn1.auth.eu-west-1.amazoncognito.com/signup?response_type=code&client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}`;
 
 document.getElementById('loginButton')?.addEventListener('click', () => {
     window.location.href = loginUrl;
