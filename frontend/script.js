6a9ccf4a214648feb954f796fd06de77748055c2
s0595910 <Kaustubh.Yawalkar@Student.HTW-Berlin.de>
2025-01-28 22:40:58 +0100

    re enabled cognito 2

--- frontend/script.js
+++ frontend/script.js
@@ -4,7 +4,7 @@ function checkAuth() {
   const idToken = localStorage.getItem('id_token');
 
   if (!accessToken || !idToken) {
-    alert('You need to log in first.');
+    // alert('You need to log in first.');
     //window.location.href = 'https://nqzilphn1.auth.eu-west-1.amazoncognito.com/login?client_id=6dj6abdhm6500vroqo6918ide6&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fd1tgztvbo79v27.cloudfront.net'; // Redirect to Cognito login page
   } else {
     console.log('User is authenticated.');
